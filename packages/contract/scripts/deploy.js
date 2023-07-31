277132307abae61fb4af23f5cabb8ab3d5c0da8a
takanoria0612 <takanorihiraki06@gmail.com>
2023-07-31 10:12:51 +0900

    upload to github

--- packages/contract/scripts/deploy.js
+++ packages/contract/scripts/deploy.js
@@ -1,20 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const main = async () => {
   const [deployer] = await hre.ethers.getSigners();
   const accountBalance = await deployer.getBalance();
-  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
-  const wavePortal = await waveContract.deployed()
 
   console.log("Deploying contracts with account: ", deployer.address);
   console.log("Account balance: ", accountBalance.toString());
-  console.log("Contract deployed to: ", wavePortal.address);
-  console.log("Contract deployed by: ", deployer.address);
+
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  /* コントラクトに資金を提供できるようにする */
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
 };
 
 const runMain = async () => {
